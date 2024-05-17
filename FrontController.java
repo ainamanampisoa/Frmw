068b9e888a71587f5072952728349fbdb8a5c1ce
ainamanampisoa <169085336+ainamanampisoa@users.noreply.github.com>
2024-05-17 11:34:17 +0300

    commit done

--- FrontController.java
+++ FrontController.java
@@ -1,80 +1,108 @@
 package mg.itu.prom16;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.reflections.Reflections;
-
 public class FrontController extends HttpServlet {
     private boolean checked = false;
-    private List<String> listeControllers = new ArrayList<>();
+    private final List<String> listeControllers = new ArrayList<>();
+    private final Set<String> verifiedClasses = new HashSet<>();
 
     @Override
-    public void init() throws ServletException {
-        super.init();
-        scanControllers();
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        scanControllers(config);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     private synchronized void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>FrontController</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>URL actuelle :</h1>");
-        out.println("<p>" + request.getRequestURL() + "</p>");
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>FrontController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>URL actuelle :</h1>");
+            out.println("<p>" + request.getRequestURL() + "</p>");
 
-        if (!checked) {
-            scanControllers();
-            checked = true;
+            if (!checked) {
+                ServletConfig config = getServletConfig();
+                scanControllers(config);
+                checked = true;
+            }
+
+            out.println("<h2>Liste des contrôleurs annotés avec @AnnotationController :</h2>");
+            for (String controller : listeControllers) {
+                out.println("<p>" + controller + "</p>");
+            }
+
+            out.println("</body>");
+            out.println("</html>");
         }
-
-        out.println("<h2>Liste des contrôleurs annotés avec @AnnotationController :</h2>");
-        for (String controller : listeControllers) {
-            out.println("<p>" + controller + "</p>");
-        }
-
-        out.println("</body>");
-        out.println("</html>");
-        out.close();
     }
 
-    private void scanControllers() {
-        ServletConfig config = getServletConfig();
+    private void scanControllers(ServletConfig config) {
         String controllerPackage = config.getInitParameter("controller-package");
-    
-        // Utilisez Reflections pour scanner le package et récupérer les classes annotées avec @AnnotationController
-        Reflections reflections = new Reflections(controllerPackage);
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(AnnotationController.class);
-    
-        // Parcourez chaque classe annotée pour récupérer les noms des classes
-        for (Class<?> clazz : annotatedClasses) {
-            // Vérifiez si la classe est annotée avec @AnnotationController
-            if (clazz.isAnnotationPresent(AnnotationController.class)) {
-                // Si oui, ajoutez le nom de la classe à la liste des contrôleurs
-                listeControllers.add(clazz.getName());
+        System.out.println("Scanning package: " + controllerPackage);
+
+        // Scanner les classes du package donné dans WEB-INF/classes
+        try {
+            String path = "WEB-INF/classes/" + controllerPackage.replace('.', '/');
+            File directory = new File(getServletContext().getRealPath(path));
+            if (directory.exists()) {
+                scanDirectory(directory, controllerPackage);
+            } else {
+                System.out.println("Directory does not exist: " + directory.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void scanDirectory(File directory, String packageName) {
+        System.out.println("Scanning directory: " + directory.getAbsolutePath());
+
+        for (File file : directory.listFiles()) {
+            System.out.println("Processing file: " + file.getName());
+
+            if (file.isDirectory()) {
+                scanDirectory(file, packageName + "." + file.getName());
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                try {
+                    Class<?> clazz = Class.forName(className);
+                    if (clazz.isAnnotationPresent(AnnotationController.class) && !verifiedClasses.contains(clazz.getName())) {
+                        AnnotationController annotation = clazz.getAnnotation(AnnotationController.class);
+                        listeControllers.add(clazz.getName() + " (" + annotation.value() + ")");
+                        verifiedClasses.add(clazz.getName());
+                        System.out.println("Added controller: " + clazz.getName());
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-    
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+
 # Frmw
+-creer AnnotationController 
+-creer FrontController avec fonction qui recupere les class depuis package controller(web.xml) de TEST
+-script pour envoyer les .jar dans lib de TEST
+
+# TEST
+
+-creer class TestController et autres en utilisant annotation du frmw
+-web.xml contenant le package controller(package de votre controller)
+(exemple:<servlet>
+            <servlet-name>FrontController</servlet-name>
+            <servlet-class>mg.itu.prom16.FrontController</servlet-class>
+            <init-param>
+                <param-name>controller-package</param-name>
+                <param-value>controller</param-value>
+            </init-param>
+         </servlet>)
+-script pour deployer dans webapps
